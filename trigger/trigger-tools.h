9b3e78b8426bcd9ca83e9621adc338ea5bcf50f3
MrMiner <syoa1996@gmail.com>
2018-11-27 02:39:59 +0900

    :sparkles: trigger-tools.h
    This header is a set of macros that free you from repeated operations!

---
+++ trigger/trigger-tools.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#define T_METHOD methodName(__FUNCTION__).c_str()
+#define T_CLASS className(__FUNCTION__).c_str()
+				
+
+inline std::string methodName(const std::string& prettyFunction)
+{
+	size_t colons = prettyFunction.find("::");
+	size_t end = prettyFunction.rfind(" ");
+
+	return prettyFunction.substr(colons + 2, prettyFunction.size()) + "()";
+}
+
+inline std::string className(const std::string& prettyFunction)
+{
+	size_t colons = prettyFunction.find("::");
+	if (colons == std::string::npos)
+		return "::";
+	size_t begin = prettyFunction.substr(0, colons).rfind(" ") + 1;
+	size_t end = colons - begin;
+
+	return prettyFunction.substr(begin, end);
+}
